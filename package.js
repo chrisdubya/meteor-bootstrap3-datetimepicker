40c33e660a81c1608ecb36ebaa72a9b5629977eb
Chris Williams <chris@niceandserious.com>
2016-02-09 15:53:11 +0000

    added new bootstrap compilation

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'chrisdubya:bootstrap-datetimepicker',
     summary: "Bootstrap 3 DateTime Picker Wrapper for Meteor.'",
-    version: "1.0.10",
+    version: "1.1.0",
     git: "https://github.com/chrisdubya/meteor-bootstrap3-datetimepicker.git"
 });
 
